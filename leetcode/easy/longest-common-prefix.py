e206a541838a932e311447ce67eb7c90eb2ff866
EnergikO <energiko71@gmail.com>
2021-11-24 15:28:52 +0300

    add some problems

---
+++ leetcode/easy/longest-common-prefix.py
@@ -0,0 +1,25 @@
+# Write a function to find the longest common prefix string amongst an array of strings.
+# If there is no common prefix, return an empty string "".
+
+
+def longestCommonPrefix(words: list) -> str:
+	""":parameter words list[str]"""
+	if not words:
+		return ''
+
+	letters = iter(list(max(words)))
+	answer = str()
+
+	while len(answer) != len(max(words)):
+		actual_letter = letters.__next__()
+
+		for word in words:
+			if not word.startswith(answer + actual_letter):
+				return answer
+
+		answer += actual_letter
+
+	return answer
+
+print(longestCommonPrefix(["flower","flow","flight"]))
+print(longestCommonPrefix([]))
---
+++ leetcode/easy/remove-duplicates-from-sorted-list.py
@@ -0,0 +1,34 @@
+# Given the head of a sorted linked list, delete all duplicates such that each element appears only once.
+# Return the linked list sorted as well.
+
+
+# Definition for singly-linked list.
+class ListNode:
+	def __init__(self, val=0, next=None):
+		self.val = val
+		self.next = next
+
+# ListNode{val: 1, next: ListNode{val: 1, next: ListNode{val: 2, next: None}}}
+
+
+def deleteDuplicates(head):
+	temp_answer = [head.val]
+	answer: ListNode
+
+	while head.next is not None:
+		head = head.next
+
+		if head.val != temp_answer[-1]:
+			temp_answer.append(head.val)
+
+	answer = ListNode(temp_answer[-1], None)
+	temp_answer.pop()
+
+	while temp_answer:
+		answer = ListNode(temp_answer[-1], answer)
+		temp_answer.pop()
+
+	return answer
+
+
+print(deleteDuplicates(ListNode(1, ListNode(1, ListNode(2, None)))))
